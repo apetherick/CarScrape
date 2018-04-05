439964d8664315012a569a30e7cae629660cd33c
Alex Petherick <alex.s.petherick@gmail.com>
2018-04-05 14:18:43 +0200

    Updates for https (ish) and not hammering the email servers

--- scrape.js
+++ scrape.js
@@ -8,24 +8,29 @@ var fullUrl = "http://www.u-pull-it.co.uk"
 
 var thefile = __dirname+'/file';
 var URLS = [
-	"http://www.u-pull-it.co.uk/search/catalogue/Breaker-Parts/yard/York/cartype/Vehicles-Under-7.5-Tonnes/make/Volkswagen/model/Golf",
-	"http://www.u-pull-it.co.uk/search/catalogue/Breaker-Parts/yard/York/cartype/Vehicles-Under-7.5-Tonnes/make/Volkswagen/model/Bora",
+	"https://www.u-pull-it.co.uk/search/catalogue/Breaker-Parts/yard/York/cartype/Vehicles-Under-7.5-Tonnes/make/Volkswagen/model/Golf",
+	"https://www.u-pull-it.co.uk/search/catalogue/Breaker-Parts/yard/York/cartype/Vehicles-Under-7.5-Tonnes/make/Volkswagen/model/Bora",
 ]
 
 URLS.forEach(function(URL){Scrape(URL)});
 
 function Scrape(URL){
-	request( URL, function( err, code, data ) {
-    	if ( err ) return;
-    
+	request( {"url":URL, "rejectUnauthorized": false}, function( err, code, data ) {
+    	if ( err ) {
+		console.log("Error making request" + err);
+		return;
+	}else{
+		console.log(URL);
+	}
     	$ = cheerio.load( data );
-    
+
     	$( "dl.lotInfo + a.viewButton" ).filter( function() {
     		var thisurl = this;
     		fs.readFile(thefile,"utf8", function (err, data) {
 			  if (err) throw err;
 			  if(data.indexOf(thisurl.attribs.href)===-1){
 			  	//email
+				console.log(config.fromEmail);
 				var transporter = nodemailer.createTransport({
 				    service: 'gmail',
 				    auth: {
@@ -33,20 +38,21 @@ function Scrape(URL){
 				        pass: config.fromPass
 				    }
 				});
-				transporter.sendMail({
-				    from: config.fromEmail,
-				    to: config.toEmail,
-				    subject: 'New Stock!',
-				    text: thisurl.attribs.href
-				});
-			  	fs.appendFile(thefile, thisurl.attribs.href+'\n', 'utf8');
-			  	console.log(thisurl.attribs.href);
+				setTimeout(function(){
+					transporter.sendMail({
+                                    		from: config.fromEmail,
+                                    		to: config.toEmail,
+                                   		subject: 'New Stock!',
+                                    		text: thisurl.attribs.href
+                                	}, function(err, info){console.log(err); console.log(info);});
+                                	fs.appendFile(thefile, thisurl.attribs.href+'\n', 'utf8');
+                                	console.log(thisurl.attribs.href);
+				}, Math.floor(Math.random() * 100000));
 			  }
 			});
     	} );
     	$( "li.pager-next a").filter(function(){
     		var thisNext = this;
-    		console.log(thisNext.attribs.href);
     		Scrape(fullUrl + thisNext.attribs.href);
     	});
 	} );
